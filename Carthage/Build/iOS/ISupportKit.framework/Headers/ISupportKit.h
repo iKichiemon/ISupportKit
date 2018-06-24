7c8a4b3cdeead312401ae43bc381bbe4bb88f476
iKichiemon <1990.iku.t@gmail.com>
2018-06-24 12:09:31 +0900

    add cocoapods and carthage

---
+++ Carthage/Build/iOS/ISupportKit.framework/Headers/ISupportKit.h
@@ -0,0 +1,19 @@
+//
+//  ISupportKit.h
+//  ISupportKit
+//
+//  Created by i_terasaka on 2018/06/24.
+//  Copyright © 2018年 kichiemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISupportKit.
+FOUNDATION_EXPORT double ISupportKitVersionNumber;
+
+//! Project version string for ISupportKit.
+FOUNDATION_EXPORT const unsigned char ISupportKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISupportKit/PublicHeader.h>
+
+
