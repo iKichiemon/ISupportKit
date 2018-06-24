988cabf9ed838d0afa57e42b5e3e91f29c9cc71e
iKichiemon <1990.iku.t@gmail.com>
2018-06-24 12:04:43 +0900

    create project

---
+++ ISupportKitTests/ISupportKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  ISupportKitTests.swift
+//  ISupportKitTests
+//
+//  Created by i_terasaka on 2018/06/24.
+//  Copyright © 2018年 kichiemon. All rights reserved.
+//
+
+import XCTest
+@testable import ISupportKit
+
+class ISupportKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
